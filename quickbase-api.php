53727b9fdf8131398181175736249f083d704cbd
arpieb <arpieb@533416.no-reply.drupal.org>
2013-04-18 17:42:59 -0400

    Initial checkin

---
+++ quickbase-api.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * @file
+ * 
+ * Contains an API wrapper class for the QuickBase HTTP API:
+ * http://www.quickbase.com/api-guide/index.html
+ */
+
+class QuickBaseAPI {
+  public function __construct() {
+    
+  }
+  public function __destruct() {
+    
+  }
+
+  // http://www.quickbase.com/api-guide/add_field.html
+  public function AddField() {}
+
+  // http://www.quickbase.com/api-guide/add_record.html
+  public function AddRecord() {}
+
+  // http://www.quickbase.com/api-guide/add_replace_dbpage.html
+  public function AddReplaceDBPage() {}
+
+  // http://www.quickbase.com/api-guide/add_user_to_role.html
+  public function AddUserToRole() {}
+
+  // http://www.quickbase.com/api-guide/authenticate.html
+  public function Authenticate() {}
+
+  // http://www.quickbase.com/api-guide/change_record_owner.html
+  public function ChangeRecordOwner() {}
+
+  // http://www.quickbase.com/api-guide/change_user_role.html
+  public function ChangeUserRole() {}
+
+  // http://www.quickbase.com/api-guide/clone_database.html
+  public function CloneDatabase() {}
+
+  // http://www.quickbase.com/api-guide/API_CopyMasterDetail.htm
+  public function CopyMasterDetail() {}
+
+  // http://www.quickbase.com/api-guide/create_database.html
+  public function CreateDatabase() {}
+
+  // http://www.quickbase.com/api-guide/create_table.html
+  public function CreateTable() {}
+
+  // http://www.quickbase.com/api-guide/delete_database.html
+  public function DeleteDatabase() {}
+
+  // http://www.quickbase.com/api-guide/delete_field.html
+  public function DeleteField() {}
+
+  // http://www.quickbase.com/api-guide/delete_record.html
+  public function DeleteRecord() {}
+
+  // http://www.quickbase.com/api-guide/do_query.html
+  public function DoQuery() {}
+
+  // http://www.quickbase.com/api-guide/do_query_count.html
+  public function DoQueryCount() {}
+
+  // http://www.quickbase.com/api-guide/edit_record.html
+  public function EditRecord() {}
+
+  // http://www.quickbase.com/api-guide/field_add_choices.html
+  public function FieldAddChoices() {}
+
+  // http://www.quickbase.com/api-guide/field_remove_choices.html
+  public function FieldRemoveChoices() {}
+
+  // http://www.quickbase.com/api-guide/find_db_by_name.html
+  public function FindDBByName() {}
+
+  // http://www.quickbase.com/api-guide/gen_add_record_form.html
+  public function GenAddRecordForm() {}
+
+  // http://www.quickbase.com/api-guide/gen_results_table.html
+  public function GenResultsTable() {}
+
+  // http://www.quickbase.com/api-guide/getancestorinfo.html
+  public function GetAncestorInfo() {}
+
+  // http://www.quickbase.com/api-guide/get_app_dtm_info.html
+  public function GetAppDTMInfo() {}
+
+  // http://www.quickbase.com/api-guide/get_db_info.html
+  public function GetDBInfo() {}
+
+  // http://www.quickbase.com/api-guide/get_db_page.html
+  public function GetDBPage() {}
+
+  // http://www.quickbase.com/api-guide/getdbvar.html
+  public function GetDBvar() {}
+
+  // http://www.quickbase.com/api-guide/getnumrecords.html
+  public function GetNumRecords() {}
+
+  // http://www.quickbase.com/api-guide/getrecordashtml.html
+  public function GetRecordAsHTML() {}
+
+  // http://www.quickbase.com/api-guide/getrecordinfo.html
+  public function GetRecordInfo() {}
+
+  // http://www.quickbase.com/api-guide/getroleinfo.html
+  public function GetRoleInfo() {}
+
+  // http://www.quickbase.com/api-guide/getschema.html
+  public function GetSchema() {}
+
+  // http://www.quickbase.com/api-guide/getuserinfo.html
+  public function GetUserInfo() {}
+
+  // http://www.quickbase.com/api-guide/getuserrole.html
+  public function GetUserRole() {}
+
+  // http://www.quickbase.com/api-guide/granteddbs.html
+  public function GrantedDBs() {}
+
+  // http://www.quickbase.com/api-guide/importfromcsv.html
+  public function ImportFromCSV() {}
+
+  // http://www.quickbase.com/api-guide/provisionuser.html
+  public function ProvisionUser() {}
+
+  // http://www.quickbase.com/api-guide/purgerecords.html
+  public function PurgeRecords() {}
+
+  // http://www.quickbase.com/api-guide/removeuserfromrole.html
+  public function RemoveUserFromRole() {}
+
+  // http://www.quickbase.com/api-guide/renameapp.html
+  public function RenameApp() {}
+
+  // http://www.quickbase.com/api-guide/runimport.html
+  public function RunImport() {}
+
+  // http://www.quickbase.com/api-guide/sendinvitation.html
+  public function SendInvitation() {}
+
+  // http://www.quickbase.com/api-guide/setdbvar.html
+  public function SetDBvar() {}
+
+  // http://www.quickbase.com/api-guide/setfieldproperties.html
+  public function SetFieldProperties() {}
+
+  // http://www.quickbase.com/api-guide/setkeyfield.html
+  public function SetKeyField() {}
+
+  // http://www.quickbase.com/api-guide/signout.html
+  public function SignOut() {}
+
+  // http://www.quickbase.com/api-guide/uploadfile.html
+  public function UploadFile() {}
+
+  // http://www.quickbase.com/api-guide/userroles.html
+  public function UserRoles() {}
+};
